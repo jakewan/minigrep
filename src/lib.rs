748444e4a53f04501efcc2ecd430a0c8195f4bfd
Jacob Wan <jacobwan840@gmail.com>
2021-12-05 15:19:00 -0800

    Further prefer iter adaptor to mutable state

--- src/lib.rs
+++ src/lib.rs
@@ -52,13 +52,10 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut r = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            r.push(line);
-        }
-    }
-    r
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
